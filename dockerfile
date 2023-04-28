d9ee31c201dc80cfd7280c7aaca5fe512f2b6215
INSide-734 <3217962725@qq.com>
2023-04-28 22:05:11 +0800

    update

--- dockerfile
+++ dockerfile
@@ -1,12 +1,12 @@
-# FROM mirekphd/python3.10-ubuntu20.04
-FROM zhenxunbot:1.0
+FROM mirekphd/python3.10-ubuntu20.04
+# FROM zhenxunbot:1.0
 ENV TZ=Asia/Shanghai
 ENV LANG zh_CN.UTF-8
 ENV LANGUAGE zh_CN.UTF-8
 ENV LC_ALL zh_CN.UTF-8
 ENV TZ Asia/Shanghai
 ADD . /root/zhenxun_bot
-# RUN apt update && apt install -y wget vim ffmpeg locales locales-all fonts-noto libnss3-dev libxss1 libasound2 libxrandr2 libatk1.0-0 libgtk-3-0 libgbm-dev libxshmfence1 git
+RUN apt update && apt install -y ffmpeg locales locales-all fonts-noto libnss3-dev libxss1 libasound2 libxrandr2 libatk1.0-0 libgtk-3-0 libgbm-dev libxshmfence1
 WORKDIR /root/zhenxun_bot
 RUN pip3 install poetry
 RUN poetry install
--- models/sign_group_user.py
+++ models/sign_group_user.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Literal, Optional, Tuple
+from typing import List, Literal, Optional, Tuple, Union
 
 from tortoise import fields
 
@@ -10,13 +10,13 @@ class SignGroupUser(Model):
 
     id = fields.IntField(pk=True, generated=True, auto_increment=True)
     """自增id"""
-    user_qq = fields.BigIntField()
+    user_id = fields.CharField(255)
     """用户id"""
-    group_id = fields.BigIntField()
+    group_id = fields.CharField(255)
     """群聊id"""
     checkin_count = fields.IntField(default=0)
     """签到次数"""
-    checkin_time_last = fields.DatetimeField(auto_now=True)
+    checkin_time_last = fields.DatetimeField(default=datetime.min)
     """最后签到时间"""
     impression = fields.DecimalField(10, 3, default=0)
     """好感度"""
@@ -29,7 +29,7 @@ class SignGroupUser(Model):
     class Meta:
         table = "sign_group_users"
         table_description = "群员签到数据表"
-        unique_together = ("user_qq", "group_id")
+        unique_together = ("user_id", "group_id")
 
     @classmethod
     async def sign(cls, user: "SignGroupUser", impression: float):
@@ -40,6 +40,7 @@ class SignGroupUser(Model):
             :param user: 用户
             :param impression: 增加的好感度
         """
+        user.checkin_time_last = datetime.now()
         user.checkin_count = user.checkin_count + 1
         user.add_probability = 0
         user.specify_probability = 0
@@ -48,8 +49,8 @@ class SignGroupUser(Model):
 
     @classmethod
     async def get_all_impression(
-        cls, group_id: Optional[int]
-    ) -> Tuple[List[int], List[int], List[float]]:
+        cls, group_id: Union[int, str]
+    ) -> Tuple[List[str],  List[float], List[str]]:
         """
         说明:
             获取该群所有用户 id 及对应 好感度
@@ -57,15 +58,23 @@ class SignGroupUser(Model):
             :param group_id: 群号
         """
         if group_id:
-            query = cls.filter(group_id=group_id)
+            query = cls.filter(group_id=str(group_id))
         else:
             query = cls
-        value_list = await query.all().values_list("user_qq", "group_id", "impression")  # type: ignore
-        qq_list = []
+        value_list = await query.all().values_list("user_id", "group_id", "impression")  # type: ignore
+        user_list = []
         group_list = []
         impression_list = []
         for value in value_list:
-            qq_list.append(value[0])
+            user_list.append(value[0])
             group_list.append(value[1])
             impression_list.append(float(value[2]))
-        return qq_list, impression_list, group_list
+        return user_list, impression_list, group_list
+
+    @classmethod
+    async def _run_script(cls):
+        return ["ALTER TABLE sign_group_users RENAME COLUMN user_qq TO user_id;",  # 将user_id改为user_id
+                "ALTER TABLE sign_group_users ALTER COLUMN user_id TYPE character varying(255);",
+                # 将user_id字段类型改为character varying(255)
+                "ALTER TABLE sign_group_users ALTER COLUMN group_id TYPE character varying(255);"
+                ]
